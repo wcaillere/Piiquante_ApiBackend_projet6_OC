a4d09aa130fa6f652d5f96842b90e7529c912040
William Caillere <wcaillere.pro@gmail.com>
2022-09-05 16:06:58 +0200

    Ajout dotenv pour sécuriser connexion MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+console.log(process.env.DB_USER);
 
 //Connexion to the DB mongoDB
-mongoose.connect('mongodb+srv://william:testapi@testopenclassrooms.9fdhbms.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@testopenclassrooms.9fdhbms.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
