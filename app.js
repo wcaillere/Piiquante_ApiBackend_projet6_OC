3fccb724b5820a99c02a256829c5aa50a1d2a442
William Caillere <wcaillere.pro@gmail.com>
2022-09-06 10:20:12 +0200

    préparation des Router

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-console.log(process.env.DB_USER);
+const authRoutes = require('./routes/auth')
+const saucesRoutes = require('./routes/sauces')
 
 //Connexion to the DB mongoDB
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@testopenclassrooms.9fdhbms.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const app = express()
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/sauces', saucesRoutes);
 
 module.exports = app;
