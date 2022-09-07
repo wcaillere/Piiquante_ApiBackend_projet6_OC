f29bd55afff94e54a134934e48014acfe9fe5e69
William Caillere <wcaillere.pro@gmail.com>
2022-09-07 11:54:38 +0200

    Ajout route GET pour récupérer toutes les sauces

---
+++ controllers/sauces.js
@@ -0,0 +1,7 @@
+const Sauce = require('../models/Sauce');
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({error}))
+}
--- routes/sauces.js
+++ routes/sauces.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const sauceCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth')
 const router = express.Router();
+
+router.get('/', auth, sauceCtrl.getAllSauces)
 
 module.exports = router;
